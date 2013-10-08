d99a6b9efa5a2fc304a84ab6488404d176a5b4d6
nicolas <n.chaulet@gmail.com>
2013-10-08 14:06:33 +0200

    remove unused import

--- DependencyInjection/NcElephantIOExtension.php
+++ DependencyInjection/NcElephantIOExtension.php
@@ -3,9 +3,7 @@
 namespace Nc\Bundle\ElephantIOBundle\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\Config\FileLocator;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
-use Symfony\Component\DependencyInjection\Loader;
 use Symfony\Component\DependencyInjection\Definition;
 use Symfony\Component\DependencyInjection\Reference;
 
